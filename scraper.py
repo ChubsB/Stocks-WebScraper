252f679ade2278a554399067f54ab48d9c34aa19
Hussain <hussainimam01@gmail.com>
2021-06-17 04:03:16 +0500

    Problem Fixed: Repeated creation of drivers and Inclusion of Headless Browser

--- scraper.py
+++ scraper.py
@@ -1,23 +1,26 @@
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
+import openpyxl
 
 stocks_list = ['AKBL', 'ANL', 'ASC', 'ASL', 'ASTL', 'ATRL', 'AVN', 'BAFL', 'BIPL', 'BOP', 'BYCO', 'DCL', 'DOL', 'EFERT', 'EPCL', 'FABL', 'FCCL', 'FCSC', 'FFBL', 'FFC', 'FFL', 'FNEL', 'GATM', 'GGL', 'GGGL', 'HASCOL', 'HUMNL', 'ICIBL', 'ICL', 'ISL', 'JSBL', 'JSCL',
                'KAPCO', 'KEL', 'KOSM', 'LOADS', 'LOTCHEM', 'MDTL', 'MLCF', 'NBP', 'NRSL', 'PACE', 'PAEL', 'PIAA', 'PIBTL', 'PIOC', 'POWER', 'PPL', 'PRL', 'PSX', 'PTC', 'SILK', 'SNGP', 'SPL', 'SSGC', 'STCL', 'STPL', 'TELE', 'TPL', 'TREET', 'TRG', 'UNITY', 'WAVES', 'WTL']
+stocks_list_test = ['KEL', 'ASC']
+days = 2
+openpyxl.load_workbook("Test.xlsx")
 
-url = "http://www.scstrade.com/stockscreening/SS_CompanySnapShotHP.aspx?symbol=POWER"
-driver = webdriver.Chrome(ChromeDriverManager().install())
-
-driver.get(url)
-content = driver.page_source
-soup = BeautifulSoup(content, "lxml")
+options = Options()
+options.headless = True
+driver = webdriver.Chrome(ChromeDriverManager().install(), options=options)
 
 for i in stocks_list:
+
     url = "http://www.scstrade.com/stockscreening/SS_CompanySnapShotHP.aspx?symbol=" + i
-    driver = webdriver.Chrome(ChromeDriverManager().install())
     driver.get(url)
     content = driver.page_source
     soup = BeautifulSoup(content, "lxml")
+
     date = driver.find_element_by_xpath(
         "/html[1]/body[1]/form[1]/div[4]/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div[1]/ul[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/table[1]/tbody[1]/tr[2]/td[1]").text
     openx = driver.find_element_by_xpath(
@@ -34,4 +37,6 @@ for i in stocks_list:
         "/html[1]/body[1]/form[1]/div[4]/div[2]/div[2]/div[2]/div[1]/div[1]/div[3]/div[1]/ul[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/table[1]/tbody[1]/tr[3]/td[5]").text
     change = round(float(close) - float(close_previous), 2)
     print(i+": ", date, openx, high, low, close, volume, change)
-    driver.close()
+    driver.implicitly_wait(1)
+
+driver.quit()
